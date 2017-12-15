28adde80b17b2a058d441a2bbd87692d8adf66b1
GoAlek <alek.golaszewski@gmail.com>
2017-12-16 00:33:03 +0100

    added basic API operations

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ mongoose.connect('mongodb://localhost:27017/bookshop');
 
 var Books = require('./models/books.js');
 
-//---->>> POST BOOKS <<<----
+//---->>> POST A BOOK <<<----
 app.post('/books', function(req, res){
   var book = req.body;
 
@@ -39,6 +39,53 @@ app.post('/books', function(req, res){
     }
     res.json(books);
   })
+});
+
+//---->>> GET BOOKS <<<----
+app.get('/books', function(req, res){
+  Books.find(function(err, books){
+    if(err){
+      throw err;
+    }
+    res.json(books);
+  })
+});
+
+//---->>> DELETE A BOOK <<<----
+app.delete('/books/:_id', function(req, res){
+  var query = {_id: req.params._id};
+
+  Books.remove(query, function(err, books){
+    if(err){
+      throw err;
+    }
+    res.json(books);
+  })
+});
+
+//---->>> UPDATE A BOOK <<<----
+app.put('/books/:_id', function(req, res){
+  var book = req.body;
+  var query = req.params._id;
+  //if the field doesn't exist $set will set a new field
+
+  var updatedBook = {
+    '$set': {
+      title: book.title,
+      description: book.description,
+      image: book.image,
+      price: book.price
+    }
+  };
+
+  var options = {new: true};
+
+  Books.findOneAndUpdate(query, updatedBook, options, function(err, books){
+    if(err){
+      throw err;
+    }
+    res.json(books);
+  });
 });
 
 // END APIs
